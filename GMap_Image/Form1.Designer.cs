7a54930fd2806ed7092822b6ee96052ff475d4eb
bluesnooker123 <dev333perfect@gmail.com>
2021-03-08 19:28:55 +0700

    Final Commit

---
+++ GMap_Image/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace GMap_Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.btnLoadIntoMap = new System.Windows.Forms.Button();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtZoom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(483, 412);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(483, 412);
+            this.map.TabIndex = 1;
+            this.map.Zoom = 0D;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "latitude:";
+            // 
+            // txtLat
+            // 
+            this.txtLat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLat.Location = new System.Drawing.Point(534, 47);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(111, 20);
+            this.txtLat.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "longitude:";
+            // 
+            // txtLong
+            // 
+            this.txtLong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLong.Location = new System.Drawing.Point(534, 102);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(111, 20);
+            this.txtLong.TabIndex = 3;
+            // 
+            // btnLoadIntoMap
+            // 
+            this.btnLoadIntoMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadIntoMap.Location = new System.Drawing.Point(534, 308);
+            this.btnLoadIntoMap.Name = "btnLoadIntoMap";
+            this.btnLoadIntoMap.Size = new System.Drawing.Size(111, 47);
+            this.btnLoadIntoMap.TabIndex = 4;
+            this.btnLoadIntoMap.Text = "Load Into Map";
+            this.btnLoadIntoMap.UseVisualStyleBackColor = true;
+            this.btnLoadIntoMap.Click += new System.EventHandler(this.btnLoadIntoMap_Click);
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Location = new System.Drawing.Point(534, 371);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(110, 44);
+            this.btnSaveImage.TabIndex = 5;
+            this.btnSaveImage.Text = "Save Map to Image";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(535, 157);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(109, 20);
+            this.txtWidth.TabIndex = 6;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(535, 212);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(109, 20);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "width:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "longitude:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(535, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "height:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(534, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Zoom";
+            // 
+            // txtZoom
+            // 
+            this.txtZoom.Location = new System.Drawing.Point(534, 267);
+            this.txtZoom.Name = "txtZoom";
+            this.txtZoom.ReadOnly = true;
+            this.txtZoom.Size = new System.Drawing.Size(109, 20);
+            this.txtZoom.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 412);
+            this.Controls.Add(this.txtZoom);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.btnLoadIntoMap);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.Button btnLoadIntoMap;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtZoom;
+    }
+}
+
---
+++ GMap_Image/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace GMap_Image
+{
+    using System.Globalization;
+    using System.Reflection;
+    using GMap.NET.Projections;
+    using GMap.NET.MapProviders;
+    using GMap.NET;
+    using GMap.NET.WindowsForms;
+    using GMap.NET.WindowsForms.Markers;
+    using GMap.NET.WindowsForms.ToolTips;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void func_ZoomChanged()
+        {
+            txtZoom.Text = map.Zoom.ToString();
+        }
+        private void btnLoadIntoMap_Click(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleSatelliteMap;
+            
+            //map.MapProvider = GMapProviders.BingMap;
+            double lat = Convert.ToDouble(txtLat.Text);
+            double longt = Convert.ToDouble(txtLong.Text);
+            map.Position = new PointLatLng(lat, longt);
+            map.MinZoom = 1;
+            map.MaxZoom = 25;
+            map.Zoom = 10;
+/*            double temp =  (360 / (Convert.ToDouble(txtWidth.Text) + Convert.ToDouble(txtHeight.Text)));
+            if (temp > 100)
+                map.Zoom = 100;
+            else if (temp < 1)
+                map.Zoom = 1;
+            txtZoom.Text = map.Zoom.ToString();
+            */
+            map.OnMapZoomChanged += new MapZoomChanged(func_ZoomChanged);
+
+
+            GMapOverlay polygons = new GMapOverlay("polygons");
+            List<PointLatLng> points = new List<PointLatLng>();
+
+            points.Add(new PointLatLng(lat - Convert.ToDouble(txtWidth.Text) / 2, longt - Convert.ToDouble(txtHeight.Text) / 2));
+            points.Add(new PointLatLng(lat - Convert.ToDouble(txtWidth.Text) / 2, longt + Convert.ToDouble(txtHeight.Text) / 2));
+            points.Add(new PointLatLng(lat + Convert.ToDouble(txtWidth.Text) / 2, longt + Convert.ToDouble(txtHeight.Text) / 2));
+            points.Add(new PointLatLng(lat + Convert.ToDouble(txtWidth.Text) / 2, longt - Convert.ToDouble(txtHeight.Text) / 2));
+            GMapPolygon polygon = new GMapPolygon(points, "MyPolygon");
+            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+            polygon.Stroke = new Pen(Color.Red, 2);
+            polygons.Polygons.Add(polygon);
+            map.Overlays.Add(polygons);
+            map.Zoom -= 1;
+            map.Zoom += 1;
+
+
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            DateTime cur = DateTime.Now;
+            Image b = map.ToImage();
+            string savePath = "Saved Image/image_" + cur.Hour.ToString() + "_"
+                + cur.Minute.ToString() + "_"
+                + cur.Second.ToString() + ".jpg";
+            b.Save(savePath, ImageFormat.Jpeg);
+        }
+    }
+}
